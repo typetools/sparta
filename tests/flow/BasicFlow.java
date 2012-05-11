681be5ba173e32b389df1bce40f305ca59e28f4a
Werner Dietl <wdietl@gmail.com>
2012-05-10 19:02:08 -0700

    Test case for Flow Checker.

---
+++ tests/flow/BasicFlow.java
@@ -0,0 +1,25 @@
+import sparta.checkers.quals.*;
+import sparta.checkers.quals.FlowSources.FlowSource;
+
+class BasicFlow {
+    Object nomic;
+    @FlowSources(FlowSource.ANY) Object any;
+
+    @FlowSources(FlowSource.MICROPHONE) Object getSound() { return null; }
+
+    void test1() {
+        // The type of the local variable is inferred to include MICROPHONE
+        Object o = getSound();
+        //:: error: (assignment.type.incompatible)
+        nomic = o;
+        any = o;
+    }
+
+    @FlowSources({FlowSource.MICROPHONE, FlowSource.CAMERA}) Object getSoundOrCam() { return getSound(); }
+
+    @FlowSources(FlowSource.ANY) Object test2() {
+        Object o = getSound();
+        o = getSoundOrCam();
+        return o;
+    }
+}
