b163b772824e6caa9058398d208e02c1f28ef265
Werner Dietl <wdietl@gmail.com>
2012-05-16 17:56:49 -0700

    Defaulting for sinks: by default everything has no sink (top), just "null" has any sink (bottom).

--- src/sparta/checkers/FlowAnnotatedTypeFactory.java
+++ src/sparta/checkers/FlowAnnotatedTypeFactory.java
@@ -3,6 +3,7 @@ package sparta.checkers;
 import java.util.Collections;
 
 import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.Tree;
 
 import checkers.quals.DefaultLocation;
 import checkers.types.BasicAnnotatedTypeFactory;
@@ -16,6 +17,11 @@ public class FlowAnnotatedTypeFactory extends BasicAnnotatedTypeFactory<FlowChec
         defaults.addAbsoluteDefault(checker.NOFLOWSOURCES, Collections.singleton(DefaultLocation.ALL_EXCEPT_LOCALS));
         // Use the top type for local variables and let flow refine the type.
         defaults.setLocalVariableDefault(Collections.singleton(checker.ANYFLOWSOURCES));
+
+        // Default is always the top annotation for sinks.
+        defaults.addAbsoluteDefault(checker.NOFLOWSINKS, Collections.singleton(DefaultLocation.ALL));
+        // But let's send null down any sink.
+        this.treeAnnotator.addTreeKind(Tree.Kind.NULL_LITERAL, checker.ANYFLOWSINKS);
     }
 
 }
