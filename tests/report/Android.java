993927d68e0cc8567c597aa48ec7213621fa865b
Werner Dietl <wdietl@gmail.com>
2012-07-23 04:16:55 -0700

    Simple test case to ensure Android packages are reported.

---
+++ tests/report/Android.java
@@ -0,0 +1,6 @@
+import android.app.Activity;
+
+// Error is actually raised twice: once for the superclass
+// and once for the default constructor invocation
+//:: error: (usage)
+class MyActivity extends Activity {}
