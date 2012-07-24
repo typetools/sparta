bea1d64c55bac54f2a4e0a7d78f8ea3158f53e12
Philip Lai <devnull@localhost>
2012-07-24 14:51:08 -0700

    Added annotations to flow.astub. Added new test file PolyTest.java to tests.

---
+++ tests/flow/PolyTest.java
@@ -0,0 +1,117 @@
+
+import android.content.ContentValues;
+import sparta.checkers.quals.*;
+import sparta.checkers.quals.FlowSources.FlowSource;
+
+class PolyTest {
+    
+	void polyPut() {
+		ContentValues values = new ContentValues();
+		@FlowSources(FlowSource.LOCATION) double input = 2.0;
+		values.put("test", input);
+	}
+	
+	/*void stringFormat() {
+		@FlowSources(FlowSource.LOCATION) double mLat = 2.0;
+		@FlowSources(FlowSource.LOCATION) double mLon = 2.0;
+		int ZOOM_LEVEL = 1;
+		@FlowSources({}) String input = "geo:%f,%f?z=%d";
+		
+		@FlowSources(FlowSource.LOCATION) String result = String.format(input, mLat, mLon, ZOOM_LEVEL);
+		
+	}*/
+	
+	/*void testMerge() {
+		@FlowSources(FlowSource.LOCATION) String a = "sdf";
+		String b = "jkl";
+		
+		
+		@FlowSources(FlowSource.LOCATION) double d = 2.0;
+		double e = 3.9;
+		
+		
+		
+		String c = PolyTest.merge("abc", d, e, 3);
+	}*/
+	
+	void test_allPrimitive_NoQualifiers() {
+		double a = 1.0;
+		double b = 2.0;
+		String c = PolyTest.merge("abc", a, b);
+	}
+	void test_onePrimitive_NoQualifiers() {
+		Double a = 1.0;
+		double b = 2.0;
+		String c = PolyTest.merge("abc", a, b);
+	}
+	void test_noPrimitive_NoQualifiers() {
+		Double a = 1.0;
+		Double b = 2.0;
+		String c = PolyTest.merge("abc", a, b);
+	}
+	
+	/*--------*/
+	
+	void test_allPrimitive_FirstQualifiers() {
+		@FlowSources(FlowSource.LOCATION) double a = 1.0;
+		double b = 2.0;
+		//:: error: (argument.type.incompatible)
+		String c = PolyTest.merge("abc", a, b);
+	}
+	void test_onePrimitive_FirstQualifiers() {
+		@FlowSources(FlowSource.LOCATION) Double a = 1.0;
+		double b = 2.0;
+		String c = PolyTest.merge("abc", a, b);
+	}
+	void test_noPrimitive_FirstQualifiers() {
+		@FlowSources(FlowSource.LOCATION) Double a = 1.0;
+		Double b = 2.0;
+		String c = PolyTest.merge("abc", a, b);
+	}
+	
+	/*--------*/
+	
+	void test_allPrimitive_SecondQualifiers() {
+		double a = 1.0;
+		@FlowSources(FlowSource.LOCATION) double b = 2.0;
+		//:: error: (argument.type.incompatible)
+		String c = PolyTest.merge("abc", a, b);
+	}
+	void test_onePrimitive_SecondQualifiers() {
+		Double a = 1.0;
+		@FlowSources(FlowSource.LOCATION) double b = 2.0;
+		//:: error: (argument.type.incompatible)
+		String c = PolyTest.merge("abc", a, b);
+	}
+	void test_noPrimitive_SecondQualifiers() {
+		Double a = 1.0;
+		@FlowSources(FlowSource.LOCATION) Double b = 2.0;
+		String c = PolyTest.merge("abc", a, b);
+	}
+	
+	/*--------*/
+	
+	void test_allPrimitive_AllQualifiers() {
+		@FlowSources(FlowSource.LOCATION) double a = 1.0;
+		@FlowSources(FlowSource.LOCATION) double b = 2.0;
+		//:: error: (argument.type.incompatible)
+		String c = PolyTest.merge("abc", a, b);
+	}
+	void test_onePrimitive_AllQualifiers() {
+		@FlowSources(FlowSource.LOCATION) Double a = 1.0;
+		@FlowSources(FlowSource.LOCATION) double b = 2.0;
+		String c = PolyTest.merge("abc", a, b);
+	}
+	void test_noPrimitive_AllQualifiers() {
+		@FlowSources(FlowSource.LOCATION) Double a = 1.0;
+		@FlowSources(FlowSource.LOCATION) Double b = 2.0;
+		String c = PolyTest.merge("abc", a, b);
+	}
+	
+	/*--------*/
+	
+	static @PolyFlowSources @PolyFlowSinks String merge(@PolyFlowSources @PolyFlowSinks String one,
+			@PolyFlowSources @PolyFlowSinks  Object... two) {
+		return one;
+	}
+}
