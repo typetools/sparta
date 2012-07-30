1f99cd4fe92711a5ce704c4f00048a92fbee42f0
Werner Dietl <wdietl@gmail.com>
2012-07-29 18:01:01 -0700

    Make test case pass.

--- tests/flow/MultiplyTest.java
+++ tests/flow/MultiplyTest.java
@@ -7,27 +7,28 @@ import android.util.FloatMath;
 class MultiplyTest {
 
     @FlowSources(FlowSource.LOCATION) float raRadians;
+
     float x;
-    @FlowSources(FlowSource.LOCATION)float y; 
-    @FlowSources(FlowSource.ANY)float z;
+    @FlowSources(FlowSource.LOCATION) float y; 
+    @FlowSources(FlowSource.ANY) float z;
+
     void test_Multiply() {
         @FlowSources(FlowSource.LOCATION) float ra = (float) 1.0;
         @FlowSources(FlowSource.LOCATION) float dec = (float) 2.0;
 
         float dtr = (float) 0.5;
 
-        
         raRadians = ra * dtr;
         float decRadians = dec * dtr;
-        
+
         //:: error: (assignment.type.incompatible)
-        //x = FloatMath.cos(raRadians) * FloatMath.cos(decRadians);
+        x = FloatMath.cos(raRadians) * FloatMath.cos(decRadians);
         y = FloatMath.sin(raRadians) * FloatMath.cos(decRadians);
         y = FloatMath.sin(y);
-        //z = FloatMath.sin(decRadians) * FloatMath.cos(decRadians);
+        z = FloatMath.sin(decRadians) * FloatMath.cos(decRadians);
     }
 
-    /*void test_Cast() {
+    void test_Cast() {
         @FlowSources(FlowSource.LOCATION) float ra = (float) 1.0;
 
         //:: warning: (cast.unsafe)
@@ -38,6 +39,8 @@ class MultiplyTest {
         @FlowSources(FlowSource.LOCATION) float ra = (float) 1.0;
 
         //:: error: (assignment.type.incompatible)
+        x = -ra;
+
         raRadians = -ra;
-    }*/
+    }
 }
