413da7f6bf9a05e7603090e8cb1e4ad09b7771d7
Philip Lai <plai68@cs.washington.edu>
2012-07-30 13:07:50 -0700

    Added MiscTests.java, which is looking at FlowSink vs FlowSource behavior with polymorphism and String.format()

---
+++ tests/flow/MiscTests.java
@@ -0,0 +1,44 @@
+import sparta.checkers.quals.*;
+import sparta.checkers.quals.FlowSources.FlowSource;
+import sparta.checkers.quals.FlowSinks.FlowSink;
+
+class MiscTests {
+    @SuppressWarnings("flow")
+	@FlowSinks(FlowSink.NETWORK) String WEBSERVICE_URL = "[...]lat=%f&lon=%f&format=24+hourly&numDays=%d";
+    
+    @FlowSources(FlowSource.LOCATION) String WEBSERVICE_URL2 = "[...]lat=%f&lon=%f&format=24+hourly&numDays=%d";
+    
+    String WEBSERVICE_URL3 = "[...]lat=%f&lon=%f&format=24+hourly&numDays=%d";
+	String result;
+	
+	void test_StringFormat_StringFlowSink() {
+		double lat = 1.0;
+		double lon = 2.0;
+		int days = 3;
+		result = String.format(WEBSERVICE_URL, lat, lon, days);
+	}
+	
+	void test_StringFormat_StringFlowSource() {
+		double lat = 1.0;
+		double lon = 2.0;
+		int days = 3;
+		result = String.format(WEBSERVICE_URL2, lat, lon, days);
+	}
+	
+	void test_StringFormat_ObjectFlowSink() {
+		@SuppressWarnings("flow")
+		@FlowSinks(FlowSink.NETWORK) double lat = 1.0;
+		@SuppressWarnings("flow")
+		@FlowSinks(FlowSink.NETWORK) double lon = 2.0;
+		@SuppressWarnings("flow")
+		@FlowSinks(FlowSink.NETWORK) int days = 3;
+		result = String.format(WEBSERVICE_URL3, lat, lon, days);
+	}
+	
+	void test_StringFormat_ObjectFlowSource() {
+		@FlowSources(FlowSource.LOCATION) double lat = 1.0;
+		@FlowSources(FlowSource.LOCATION) double lon = 2.0;
+		@FlowSources(FlowSource.LOCATION) int days = 3;
+		result = String.format(WEBSERVICE_URL3, lat, lon, days);
+	}
+}
