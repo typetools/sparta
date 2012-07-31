4d8fd9f5dea4f3112a00c7d25aef20444722a482
Philip Lai <plai68@cs.washington.edu>
2012-07-30 17:44:39 -0700

    Commented out extra methods

--- tests/flow/MiscTests.java
+++ tests/flow/MiscTests.java
@@ -3,14 +3,14 @@ import sparta.checkers.quals.FlowSources.FlowSource;
 import sparta.checkers.quals.FlowSinks.FlowSink;
 
 class MiscTests {
-    @SuppressWarnings("flow")
+	@SuppressWarnings("flow")
 	@FlowSinks(FlowSink.NETWORK) String WEBSERVICE_URL = "[...]lat=%f&lon=%f&format=24+hourly&numDays=%d";
     
     @FlowSources(FlowSource.LOCATION) String WEBSERVICE_URL2 = "[...]lat=%f&lon=%f&format=24+hourly&numDays=%d";
     
     String WEBSERVICE_URL3 = "[...]lat=%f&lon=%f&format=24+hourly&numDays=%d";
 	String result;
-	
+	/*
 	void test_StringFormat_StringFlowSink() {
 		double lat = 1.0;
 		double lon = 2.0;
@@ -22,23 +22,25 @@ class MiscTests {
 		double lat = 1.0;
 		double lon = 2.0;
 		int days = 3;
+		//::error: (assignment.type.incompatible)
 		result = String.format(WEBSERVICE_URL2, lat, lon, days);
 	}
-	
+	*/
 	void test_StringFormat_ObjectFlowSink() {
 		@SuppressWarnings("flow")
-		@FlowSinks(FlowSink.NETWORK) double lat = 1.0;
+		@FlowSinks(FlowSink.NETWORK) @FlowSources double lat = 1.0;
 		@SuppressWarnings("flow")
-		@FlowSinks(FlowSink.NETWORK) double lon = 2.0;
+		@FlowSinks(FlowSink.NETWORK) @FlowSources double lon = 2.0;
 		@SuppressWarnings("flow")
-		@FlowSinks(FlowSink.NETWORK) int days = 3;
+		@FlowSinks(FlowSink.NETWORK) @FlowSources int days = 3;
 		result = String.format(WEBSERVICE_URL3, lat, lon, days);
 	}
-	
+	/*
 	void test_StringFormat_ObjectFlowSource() {
 		@FlowSources(FlowSource.LOCATION) double lat = 1.0;
 		@FlowSources(FlowSource.LOCATION) double lon = 2.0;
 		@FlowSources(FlowSource.LOCATION) int days = 3;
+		//::error: (assignment.type.incompatible)
 		result = String.format(WEBSERVICE_URL3, lat, lon, days);
-	}
+	}*/
 }
