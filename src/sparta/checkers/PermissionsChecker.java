15e377cb07b236cad6e51e951dc660dac29e4632
Werner Dietl <wdietl@gmail.com>
2012-08-05 22:17:22 -0700

    Use the new permission.astub file.

--- src/sparta/checkers/PermissionsChecker.java
+++ src/sparta/checkers/PermissionsChecker.java
@@ -1,8 +1,10 @@
 package sparta.checkers;
 
 import checkers.basetype.BaseTypeChecker;
+import checkers.quals.StubFiles;
 import checkers.quals.TypeQualifiers;
 import checkers.quals.Unqualified;
 
 @TypeQualifiers(Unqualified.class)
+@StubFiles("permission.astub")
 public class PermissionsChecker extends BaseTypeChecker {}
