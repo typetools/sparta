e1a4a1c527dcf0c6410c6fa51102d298c1d022dd
Philip Lai <plai68@cs.washington.edu>
2012-08-08 14:31:02 -0700

    Added annotations to flow.astub. Added a second test to tests/flow/TestPolyResolve.java

--- tests/flow/TestPolyResolve.java
+++ tests/flow/TestPolyResolve.java
@@ -1,6 +1,14 @@
+import org.apache.http.*;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.*;
+import java.io.*;
+
+
 import sparta.checkers.quals.*;
 import sparta.checkers.quals.FlowSources.FlowSource;
 import sparta.checkers.quals.FlowSinks.FlowSink;
+
 
 class TestPolyResolve {
     float x1 = 2f;
@@ -12,4 +20,19 @@ class TestPolyResolve {
     void mod2pi(@PolyFlowSources @PolyFlowSinks float x) {
         factor = x / x1;
     }
+}
+
+class TestPolyConstructors {
+	private @FlowSources(FlowSource.LOCATION) HttpResponse response;
+	private @FlowSources(FlowSource.LOCATION) HttpEntity entity;
+	private @FlowSources(FlowSource.LOCATION) InputStream tempStream;
+	private Reader responseReader;
+	
+	void sdf(@FlowSinks(FlowSink.NETWORK) @FlowSources(FlowSource.LOCATION) HttpGet request) {
+		HttpClient client = new DefaultHttpClient();
+		response = client.execute(request);
+		entity = response.getEntity();
+		tempStream = entity.getContent();
+		responseReader = new InputStreamReader(tempStream);
+	}
 }
