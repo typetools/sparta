1b2f822fd262e95a2e8e8de3a8c941ea1e898371
Philip Lai <plai68@cs.washington.edu>
2012-08-07 17:29:23 -0700

    Make Eclipse happy by adding more Target locations.

--- src/sparta/checkers/quals/PolyFlowSinks.java
+++ src/sparta/checkers/quals/PolyFlowSinks.java
@@ -9,7 +9,9 @@ import checkers.quals.*;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE_PARAMETER, ElementType.TYPE_USE})
+@Target({ElementType.TYPE_PARAMETER, ElementType.TYPE_USE, 
+    /* The following only added to make Eclipse work. */
+    ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD, ElementType.LOCAL_VARIABLE})
 @TypeQualifier
 @PolymorphicQualifier(FlowSinks.class)
 public @interface PolyFlowSinks {}
