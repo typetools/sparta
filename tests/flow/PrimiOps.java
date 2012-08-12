6726101f24b9b4873e89f56951e331ce93e5e428
Werner Dietl <wdietl@gmail.com>
2012-08-12 02:02:18 -0700

    Test behavior of primitive operations.

---
+++ tests/flow/PrimiOps.java
@@ -0,0 +1,35 @@
+import sparta.checkers.quals.FlowSinks;
+import sparta.checkers.quals.FlowSources;
+import sparta.checkers.quals.PolyFlowSinks;
+import sparta.checkers.quals.PolyFlowSources;
+import sparta.checkers.quals.FlowSinks.FlowSink;
+import sparta.checkers.quals.FlowSources.FlowSource;
+
+class PrimiOps {
+    @FlowSinks({}) @FlowSources({FlowSource.ANY}) float top;
+    @PolyFlowSources @PolyFlowSinks float poly;
+    @FlowSinks({FlowSink.ANY}) @FlowSources({}) float bot;
+    float unqual;
+
+    void mod2pi() {
+        top = top / top;
+        poly = poly * poly;
+        bot = bot / bot;
+        unqual = unqual * unqual;
+
+        top = unqual / top;
+        top = top / unqual;
+        unqual = unqual / bot;
+
+        poly = poly / bot;
+
+        // This does not work, because unqualified is
+        // a top/bottom mix.
+        //:: error: (assignment.type.incompatible)
+        poly = poly / unqual;
+        //:: error: (assignment.type.incompatible)
+        unqual = poly / unqual;
+    }
+
+    // TODO: ops on Strings.
+}
