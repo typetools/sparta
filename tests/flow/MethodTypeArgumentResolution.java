7d4f3219c103cafbc3a7290cc853641c3d032c2e
Werner Dietl <wdietl@gmail.com>
2012-08-12 17:28:53 -0700

    Skip a test that need further investigation.

--- tests/flow/MethodTypeArgumentResolution.java
+++ tests/flow/MethodTypeArgumentResolution.java
@@ -2,11 +2,13 @@ import sparta.checkers.quals.*;
 import sparta.checkers.quals.FlowSources.FlowSource;
 import java.util.*;
 
+// TODO: I couldn't reproduce this issue in the PolyAll Checker.
+//@skip-test
 class MethodTypeArgumentResolution {
-	void foo(List<@FlowSources(FlowSource.LOCATION) Object> l, Comparator<@FlowSources(FlowSource.LOCATION) Object> c) {
-		
-		//Unexpected incompatible type error, because method type argument is not inferred correctly.
-		Collections.sort(l, c);
-		Collections.</*@FlowSources(FlowSource.LOCATION)*/ Object>sort(l, c);
-	}
+    void foo(List<@FlowSources(FlowSource.LOCATION) Object> l, Comparator<@FlowSources(FlowSource.LOCATION) Object> c) {
+        // Unexpected incompatible type error, because method type argument is not inferred correctly.
+        Collections.sort(l, c);
+        // This call is/should be equivalent.
+        Collections.<@FlowSources(FlowSource.LOCATION) Object>sort(l, c);
+    }
 }
