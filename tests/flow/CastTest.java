4f819c5f92a84f455107b3a8f182ab26ee12c708
Werner Dietl <wdietl@gmail.com>
2012-08-23 01:45:15 -0700

    Small adaptations to test case.

--- tests/flow/CastTest.java
+++ tests/flow/CastTest.java
@@ -4,20 +4,21 @@ import sparta.checkers.quals.PolyFlowSinks;
 import sparta.checkers.quals.PolyFlowSources;
 
 class CastTest {
-	@FlowSources(FlowSource.LOCATION) double result;
-	@FlowSources(FlowSource.LOCATION) int x;
-  
-	void m() {
-    	x = 2;
-    	result = foo(x);
-    	result = bar(x);
-	}
-	
-	public @FlowSources(FlowSource.LOCATION) double foo(@FlowSources(FlowSource.LOCATION) int x) {
-	    return (@FlowSources(FlowSource.LOCATION) double) x;
-	}
-	
-	public @FlowSources(FlowSource.LOCATION) double bar(@FlowSources(FlowSource.LOCATION) int x) {
-	    return (double) x;
-	}
+    @FlowSources(FlowSource.LOCATION) double result;
+    @FlowSources(FlowSource.LOCATION) int x;
+
+    void m() {
+        result = bar(x);
+        //:: error: (argument.type.incompatible)
+        result = foo(x);
+    }
+
+    public @FlowSources(FlowSource.LOCATION) double bar(@FlowSources(FlowSource.LOCATION) int x) {
+        return (double) x;
+    }
+
+    public @FlowSources(FlowSource.LOCATION) double foo(int x) {
+        return (@FlowSources(FlowSource.LOCATION) double) x;
+    }
+
 }
