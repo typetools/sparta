db4e58873f0cad7c7af6d72db3fab07c5b03cff5
Werner Dietl <wdietl@gmail.com>
2012-08-25 14:41:05 -0700

    Cleanup and correction of test case.

--- tests/flow/ArrayCast.java
+++ tests/flow/ArrayCast.java
@@ -6,21 +6,24 @@ import sparta.checkers.quals.PolyFlowSinks;
 import sparta.checkers.quals.PolyFlowSources;
 
 class ArrayCast {
-    
+
     void foo() {
- 	   @SuppressWarnings("flow")
- 	   @FlowSinks(FlowSink.NETWORK) Object[] params = new /*@FlowSinks(FlowSink.NETWORK)*/ Object[1]; 
- 	   Object[] result = (Object[]) call("method", params);
- 	   callFinished(result);
+        @SuppressWarnings("flow")
+        @FlowSinks(FlowSink.NETWORK) Object[] params = new /*@FlowSinks(FlowSink.NETWORK)*/ Object[1]; 
+        Object[] result = (Object[]) call("method", params);
+
+        // The annotations are on the array type, not on the array component type.
+        //:: error: (argument.type.incompatible)
+        callStart(result);
+        callFinished(result);
     }
-     
+
     @FlowSources(FlowSource.NETWORK) @FlowSinks(FlowSink.NETWORK) Object call(
- 		   String method, @FlowSinks(FlowSink.NETWORK) Object[] params) {
- 	   @FlowSources(FlowSource.NETWORK) @FlowSinks(FlowSink.NETWORK) Object a = params[0];
- 	   return a;
+            String method, @FlowSinks(FlowSink.NETWORK) Object[] params) {
+        @FlowSources(FlowSource.NETWORK) @FlowSinks(FlowSink.NETWORK) Object a = params[0];
+        return a;
     }
-    
-     void callFinished(@FlowSources(FlowSource.NETWORK) @FlowSinks(FlowSink.NETWORK) Object[] result) {
-     	
-     }
+
+    void callStart(@FlowSources(FlowSource.NETWORK) @FlowSinks(FlowSink.NETWORK) Object [] result) {}
+    void callFinished(Object @FlowSources(FlowSource.NETWORK) @FlowSinks(FlowSink.NETWORK) [] result) {}
 }
