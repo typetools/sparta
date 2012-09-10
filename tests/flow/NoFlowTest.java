35924f1a6ac960984288fe035e26a84f37169e3d
Philip Lai <plai68@cs.washington.edu>
2012-09-10 16:20:46 -0700

    Added a new test for @NoFlow: tests/flow/NoFlowTest.java Added a single annotation to flow.astub

---
+++ tests/flow/NoFlowTest.java
@@ -0,0 +1,63 @@
+import sparta.checkers.quals.*;
+import sparta.checkers.quals.FlowSources.FlowSource;
+import sparta.checkers.quals.FlowSinks.FlowSink;
+
+class NoFlowTest {
+	@FlowSources(FlowSource.NETWORK) int source = 1;
+	@SuppressWarnings("flow") //Variable initialization
+	@FlowSources({}) @FlowSinks(FlowSink.NETWORK) int sink = 2;
+	@SuppressWarnings("flow") //Variable initialization
+	@FlowSources(FlowSource.NETWORK) @FlowSinks(FlowSink.NETWORK) int sourceSink = 3;
+	@FlowSources({}) @FlowSinks({}) int none = 4;
+	
+	@FlowSources(FlowSource.NETWORK) TestClass classSource;
+	@SuppressWarnings("flow") //Variable initialization
+	@FlowSinks(FlowSink.NETWORK) TestClass classSink;
+	@SuppressWarnings("flow") //Variable initialization
+	@FlowSources(FlowSource.NETWORK) @FlowSinks(FlowSink.NETWORK) TestClass classSourceSink;
+	@FlowSources({}) @FlowSinks({}) TestClass classNone;
+	
+	//test on return, receiver, and whole class
+	void testParameter() {
+		 //:: error: (argument.type.incompatible)
+		testMethod(source);
+		testMethod(sink);
+		 //:: error: (argument.type.incompatible)
+		testMethod(sourceSink);
+		testMethod(none);
+	}
+	
+	void testReturn() {
+		source = testMethod(0);
+		//:: error: (assignment.type.incompatible)
+		sink = testMethod(0);
+		//:: error: (assignment.type.incompatible)
+		sourceSink = testMethod(0);
+		none = testMethod(0);
+	}
+	
+	void testReceiver() {
+		none = classSource.testClassMethod(none);
+		none = classSink.testClassMethod(none);
+		none = classSourceSink.testClassMethod(none);
+		none = classNone.testClassMethod(none);
+	}
+	
+	
+	void testClass() {
+		source = classNone.testClassMethod(source);
+		sink = classNone.testClassMethod(sink);
+		sourceSink = classNone.testClassMethod(sourceSink);
+		none = classNone.testClassMethod(none);
+	}
+	
+	@NoFlow int testMethod(int input) {
+		return 0;
+	}
+}
+
+@NoFlow class TestClass {
+	int testClassMethod(int input) {
+		return 0;
+	}
+}
