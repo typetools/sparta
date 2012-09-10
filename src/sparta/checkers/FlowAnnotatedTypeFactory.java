7ca873e85fdad86676d9ed42a23fd1074af868ec
Werner Dietl <wdietl@gmail.com>
2012-09-10 02:38:50 -0700

    Change the default qualifier for the "android" package. Support @NoFlow qualifier. Adapt to framework changes.

--- src/sparta/checkers/FlowAnnotatedTypeFactory.java
+++ src/sparta/checkers/FlowAnnotatedTypeFactory.java
@@ -1,28 +1,51 @@
 package sparta.checkers;
 
-import java.util.Collections;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.PackageElement;
+
+import sparta.checkers.quals.NoFlow;
 
 import com.sun.source.tree.CompilationUnitTree;
 import com.sun.source.tree.Tree;
 
 import checkers.quals.DefaultLocation;
+import checkers.types.AnnotatedTypeMirror;
 import checkers.types.BasicAnnotatedTypeFactory;
+import checkers.util.QualifierDefaults.DefaultApplier;
+
 
 public class FlowAnnotatedTypeFactory extends BasicAnnotatedTypeFactory<FlowChecker> {
 
     public FlowAnnotatedTypeFactory(FlowChecker checker, CompilationUnitTree root) {
         super(checker, root);
 
+        PackageElement android = env.getElementUtils().getPackageElement("android");
+
         // Use the bottom type as default for everything but local variables.
-        defaults.addAbsoluteDefault(checker.NOFLOWSOURCES, Collections.singleton(DefaultLocation.ALL_EXCEPT_LOCALS));
+        defaults.addAbsoluteDefault(checker.NOFLOWSOURCES, DefaultLocation.OTHERWISE);
         // Use the top type for local variables and let flow refine the type.
-        defaults.setLocalVariableDefault(Collections.singleton(checker.ANYFLOWSOURCES));
+        defaults.addAbsoluteDefault(checker.ANYFLOWSOURCES, DefaultLocation.LOCALS);
+        // Use the top type for return types in the Android packages
+        defaults.addElementDefault(android, checker.ANYFLOWSOURCES, DefaultLocation.RETURNS);
 
         // Default is always the top annotation for sinks.
-        defaults.addAbsoluteDefault(checker.NOFLOWSINKS, Collections.singleton(DefaultLocation.ALL));
+        defaults.addAbsoluteDefault(checker.NOFLOWSINKS, DefaultLocation.ALL);
         // But let's send null down any sink.
         this.treeAnnotator.addTreeKind(Tree.Kind.NULL_LITERAL, checker.ANYFLOWSINKS);
 
         this.postInit();
+    }
+
+    @Override
+    protected void postAsMemberOf(AnnotatedTypeMirror type,
+            AnnotatedTypeMirror owner, Element element) {
+
+        if (this.getDeclAnnotation(element, NoFlow.class) != null) {
+            // TODO: does this correctly work with NoFlow on a class or package?
+            new DefaultApplier(element, DefaultLocation.RETURNS, type).scan(type, checker.NOFLOWSOURCES);
+            // new DefaultApplier(element, DefaultLocation.PARAMETERS, type).scan(type, checker.NOFLOWSOURCES);
+        }
+
+        super.postAsMemberOf(type, owner, element);
     }
 }
