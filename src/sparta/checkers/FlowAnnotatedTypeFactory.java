ef6cab3faba2b42b2efc3057795496109a1833c6
Werner Dietl <wdietl@gmail.com>
2012-09-10 18:28:46 -0700

    Add more packages that should use conservative defaults. Fix ordering of default application to make @NoFlow actually work. Look up the Element tree to look for @NoFlow annotations.

--- src/sparta/checkers/FlowAnnotatedTypeFactory.java
+++ src/sparta/checkers/FlowAnnotatedTypeFactory.java
@@ -1,7 +1,10 @@
 package sparta.checkers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.lang.model.element.Element;
-import javax.lang.model.element.PackageElement;
+import javax.lang.model.util.Elements;
 
 import sparta.checkers.quals.NoFlow;
 
@@ -11,6 +14,7 @@ import com.sun.source.tree.Tree;
 import checkers.quals.DefaultLocation;
 import checkers.types.AnnotatedTypeMirror;
 import checkers.types.BasicAnnotatedTypeFactory;
+import checkers.util.InternalUtils;
 import checkers.util.QualifierDefaults.DefaultApplier;
 
 
@@ -19,14 +23,34 @@ public class FlowAnnotatedTypeFactory extends BasicAnnotatedTypeFactory<FlowChec
     public FlowAnnotatedTypeFactory(FlowChecker checker, CompilationUnitTree root) {
         super(checker, root);
 
-        PackageElement android = env.getElementUtils().getPackageElement("android");
+        // The list of Elements that should be treated more conservatively.
+        List<Element> conservative = new ArrayList<Element>();
+        Elements elemutils = env.getElementUtils();
+        conservative.add(elemutils.getPackageElement("android"));
+        conservative.add(elemutils.getPackageElement("com.android"));
+        conservative.add(elemutils.getPackageElement("org.apache.http"));
+        conservative.add(elemutils.getPackageElement("java"));
+        /* Is using all of java.* too conservative? At least the following
+         * packages should be conservative:
+        conservative.add(elemutils.getPackageElement("java.io"));
+        conservative.add(elemutils.getPackageElement("java.net"));
+        conservative.add(elemutils.getPackageElement("java.sql"));
+        conservative.add(elemutils.getPackageElement("java.util"));
+        // We might want to exclude java.lang.
+         * TODO: java.lang.reflect should be conservative.
+        conservative.add(elemutils.getPackageElement("java.lang"));
+        */
+
 
         // Use the bottom type as default for everything but local variables.
         defaults.addAbsoluteDefault(checker.NOFLOWSOURCES, DefaultLocation.OTHERWISE);
         // Use the top type for local variables and let flow refine the type.
         defaults.addAbsoluteDefault(checker.ANYFLOWSOURCES, DefaultLocation.LOCALS);
-        // Use the top type for return types in the Android packages
-        defaults.addElementDefault(android, checker.ANYFLOWSOURCES, DefaultLocation.RETURNS);
+
+        for (Element cons : conservative) {
+            // Use the top type for return types in the Android packages
+            defaults.addElementDefault(cons, checker.ANYFLOWSOURCES, DefaultLocation.RETURNS);
+        }
 
         // Default is always the top annotation for sinks.
         defaults.addAbsoluteDefault(checker.NOFLOWSINKS, DefaultLocation.ALL);
@@ -36,16 +60,37 @@ public class FlowAnnotatedTypeFactory extends BasicAnnotatedTypeFactory<FlowChec
         this.postInit();
     }
 
-    @Override
-    protected void postAsMemberOf(AnnotatedTypeMirror type,
-            AnnotatedTypeMirror owner, Element element) {
+    // Test whether the element or any of its enclosing elements has
+    // the NoFlow annotation.
+    private boolean isNoFlowElement(Element element) {
+        while (element != null) {
+            if (this.getDeclAnnotation(element, NoFlow.class) != null) {
+                return true;
+            }
+            element = element.getEnclosingElement();
+        }
+        return false;
+    }
 
-        if (this.getDeclAnnotation(element, NoFlow.class) != null) {
-            // TODO: does this correctly work with NoFlow on a class or package?
+    @Override
+    protected void annotateImplicit(Tree tree, AnnotatedTypeMirror type) {
+        Element element = InternalUtils.symbol(tree);
+        if (element != null && isNoFlowElement(element)) {
             new DefaultApplier(element, DefaultLocation.RETURNS, type).scan(type, checker.NOFLOWSOURCES);
             // new DefaultApplier(element, DefaultLocation.PARAMETERS, type).scan(type, checker.NOFLOWSOURCES);
         }
 
-        super.postAsMemberOf(type, owner, element);
+        super.annotateImplicit(tree, type);
     }
+
+    @Override
+    protected void annotateImplicit(Element element, AnnotatedTypeMirror type) {
+        if (element != null && isNoFlowElement(element)) {
+            new DefaultApplier(element, DefaultLocation.RETURNS, type).scan(type, checker.NOFLOWSOURCES);
+            // new DefaultApplier(element, DefaultLocation.PARAMETERS, type).scan(type, checker.NOFLOWSOURCES);
+        }
+
+        super.annotateImplicit(element, type);
+    }
+
 }
