527b37dba765a304202b1074243f7d08e782b92f
Werner Dietl <wdietl@gmail.com>
2012-09-10 20:50:16 -0700

    Use @ConservativeFlow in addition to @NoFlow to declarative handle what packages/classes/methods should have what default.

--- src/sparta/checkers/FlowAnnotatedTypeFactory.java
+++ src/sparta/checkers/FlowAnnotatedTypeFactory.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.lang.model.element.Element;
 import javax.lang.model.util.Elements;
 
+import sparta.checkers.quals.ConservativeFlow;
 import sparta.checkers.quals.NoFlow;
 
 import com.sun.source.tree.CompilationUnitTree;
@@ -23,34 +24,10 @@ public class FlowAnnotatedTypeFactory extends BasicAnnotatedTypeFactory<FlowChec
     public FlowAnnotatedTypeFactory(FlowChecker checker, CompilationUnitTree root) {
         super(checker, root);
 
-        // The list of Elements that should be treated more conservatively.
-        List<Element> conservative = new ArrayList<Element>();
-        Elements elemutils = env.getElementUtils();
-        conservative.add(elemutils.getPackageElement("android"));
-        conservative.add(elemutils.getPackageElement("com.android"));
-        conservative.add(elemutils.getPackageElement("org.apache.http"));
-        conservative.add(elemutils.getPackageElement("java"));
-        /* Is using all of java.* too conservative? At least the following
-         * packages should be conservative:
-        conservative.add(elemutils.getPackageElement("java.io"));
-        conservative.add(elemutils.getPackageElement("java.net"));
-        conservative.add(elemutils.getPackageElement("java.sql"));
-        conservative.add(elemutils.getPackageElement("java.util"));
-        // We might want to exclude java.lang.
-         * TODO: java.lang.reflect should be conservative.
-        conservative.add(elemutils.getPackageElement("java.lang"));
-        */
-
-
         // Use the bottom type as default for everything but local variables.
         defaults.addAbsoluteDefault(checker.NOFLOWSOURCES, DefaultLocation.OTHERWISE);
         // Use the top type for local variables and let flow refine the type.
         defaults.addAbsoluteDefault(checker.ANYFLOWSOURCES, DefaultLocation.LOCALS);
-
-        for (Element cons : conservative) {
-            // Use the top type for return types in the Android packages
-            defaults.addElementDefault(cons, checker.ANYFLOWSOURCES, DefaultLocation.RETURNS);
-        }
 
         // Default is always the top annotation for sinks.
         defaults.addAbsoluteDefault(checker.NOFLOWSINKS, DefaultLocation.ALL);
@@ -60,37 +37,35 @@ public class FlowAnnotatedTypeFactory extends BasicAnnotatedTypeFactory<FlowChec
         this.postInit();
     }
 
-    // Test whether the element or any of its enclosing elements has
-    // the NoFlow annotation.
-    private boolean isNoFlowElement(Element element) {
-        while (element != null) {
-            if (this.getDeclAnnotation(element, NoFlow.class) != null) {
-                return true;
-            }
-            element = element.getEnclosingElement();
-        }
-        return false;
-    }
-
     @Override
     protected void annotateImplicit(Tree tree, AnnotatedTypeMirror type) {
         Element element = InternalUtils.symbol(tree);
-        if (element != null && isNoFlowElement(element)) {
-            new DefaultApplier(element, DefaultLocation.RETURNS, type).scan(type, checker.NOFLOWSOURCES);
-            // new DefaultApplier(element, DefaultLocation.PARAMETERS, type).scan(type, checker.NOFLOWSOURCES);
-        }
-
+        handleDefaulting(element, type);
         super.annotateImplicit(tree, type);
     }
 
     @Override
     protected void annotateImplicit(Element element, AnnotatedTypeMirror type) {
-        if (element != null && isNoFlowElement(element)) {
-            new DefaultApplier(element, DefaultLocation.RETURNS, type).scan(type, checker.NOFLOWSOURCES);
-            // new DefaultApplier(element, DefaultLocation.PARAMETERS, type).scan(type, checker.NOFLOWSOURCES);
-        }
-
+        handleDefaulting(element, type);
         super.annotateImplicit(element, type);
+    }
+
+    protected void handleDefaulting(Element element, AnnotatedTypeMirror type) {
+        while (element != null) {
+            if (this.getDeclAnnotation(element, NoFlow.class) != null) {
+                // Use no flow sources for the return type.
+                new DefaultApplier(element, DefaultLocation.RETURNS, type).scan(type, checker.NOFLOWSOURCES);
+                // Nothing needs to be done for parameters.
+                // new DefaultApplier(element, DefaultLocation.PARAMETERS, type).scan(type, checker.NOFLOWSOURCES);
+                return;
+            } else if (this.getDeclAnnotation(element, ConservativeFlow.class) != null) {
+                // Use the top type for return types in the Android packages
+                defaults.addElementDefault(element, checker.ANYFLOWSOURCES, DefaultLocation.RETURNS);
+                // Nothing needs to be done for parameters.
+                // new DefaultApplier(element, DefaultLocation.PARAMETERS, type).scan(type, checker.NOFLOWSOURCES);
+            }
+            element = element.getEnclosingElement();
+        }
     }
 
 }
