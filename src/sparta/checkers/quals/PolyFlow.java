cc19db36a2ca14dfe33226aba4d264c2f758b33b
Suzanne Millstein <smillst@cs.washington.edu>
2013-02-19 16:58:27 -0800

    In flow.astub, changed @NoFlow to @PolyFlow

---
+++ src/sparta/checkers/quals/PolyFlow.java
@@ -0,0 +1,17 @@
+package sparta.checkers.quals;
+
+import java.lang.annotation.*;
+
+/**
+ * A declaration annotation to mark that no flow happens.
+ * This is shorthand for specifying no flow sources and
+ * no flow sinks for the return type and all parameter types of
+ * any contained methods.
+ *
+ * @see ConservativeFlow
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR,
+    ElementType.TYPE, ElementType.PACKAGE})
+public @interface PolyFlow {}
