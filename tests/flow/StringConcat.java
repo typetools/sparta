a8ccd9e556aab036d926b53b2e73f98a064d0d9f
Suzanne Millstein <smillst@cs.washington.edu>
2013-05-01 09:34:02 -0700

    Fixed the failing test case.

--- tests/flow/StringConcat.java
+++ tests/flow/StringConcat.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 import android.content.Context;
 import sparta.checkers.quals.*;
 import sparta.checkers.quals.FlowSources.*;
@@ -8,17 +10,11 @@ class Use {
 
     void demo() {
        @FlowSources(FlowSource.LITERAL) Context ctx = null;
-    	/*@FlowSources(FlowSource.FILESYSTEM) @FlowSinks(FlowSink.ANY) File getDir(
-         *                           @FlowSources(FlowSource.ANY) @FlowSinks(FlowSink.FILESYSTEM) String name, 
-         *                           @FlowSources(FlowSource.ANY) @FlowSinks(FlowSink.CONDITIONAL) int mode
-         *                           );
-         */
-    	// class File {	@PolyFlowSources @PolyFlowSinks String toString() @PolyFlowSources @PolyFlowSinks;}
-
-    	//ctx.getDir("log", Context.MODE_PRIVATE).toString() is FILESYSTEM->ANY
-    	//test.jpg 
-    	//I'm not sure if this error is a bug or as expected?
-    	String filename = ctx.getDir("log", 0).toString() +  "test.jpg";    
+    	//:: error: (forbidden.flow)
+       File file = ctx.getDir("log", 0);
+      // String fileString = file.toString();
+   	   //:: error: (forbidden.flow)
+       String filename = file +  "test.jpg";    
                                    
     }
 
