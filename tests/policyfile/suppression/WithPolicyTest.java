50c3ba227d2bd92aa3a7580d4356d2aa8b43ca68
Suzanne Millstein <smillst@cs.washington.edu>
2013-05-01 11:16:52 -0700

    Renamed @FlowSinks to @Sinks and @FlowSources to @Sources
    In tests/policyfile/ using XCode

--- tests/policyfile/suppression/WithPolicyTest.java
+++ tests/policyfile/suppression/WithPolicyTest.java
@@ -1,7 +1,7 @@
 import sparta.checkers.quals.*;
-import sparta.checkers.quals.FlowSinks;
-import sparta.checkers.quals.FlowSources.FlowSource;
-import sparta.checkers.quals.FlowSinks.FlowSink;
+import sparta.checkers.quals.Sinks;
+import sparta.checkers.quals.Sources.FlowSource;
+import sparta.checkers.quals.Sinks.FlowSink;
 
 import java.util.List;
 import java.io.File;
@@ -14,89 +14,89 @@ import java.io.File;
 
 
 
-@FlowSources({FlowSource.PHONE_NUMBER}) @FlowSinks({FlowSink.NETWORK})
+@Sources({FlowSource.PHONE_NUMBER}) @Sinks({FlowSink.NETWORK})
 class PolicyTest {
 
-    private @FlowSources({FlowSource.ANY}) Object anySource = new @FlowSources({FlowSource.ANY}) Object();
-    private @FlowSources({FlowSource.EXTERNAL_STORAGE, FlowSource.FILESYSTEM, FlowSource.LOCATION})
-    @FlowSinks({FlowSink.LOG})
+    private @Sources({FlowSource.ANY}) Object anySource = new @Sources({FlowSource.ANY}) Object();
+    private @Sources({FlowSource.EXTERNAL_STORAGE, FlowSource.FILESYSTEM, FlowSource.LOCATION})
+    @Sinks({FlowSink.LOG})
 //	//:: error: (forbidden.flow)
     Object esFsLocSource = null;
   //  //:: error: (forbidden.flow)
-    private @FlowSinks({FlowSink.LOG}) Object logcatSink = null;
+    private @Sinks({FlowSink.LOG}) Object logcatSink = null;
     ////:: error: (forbidden.flow)
-    private @FlowSources({FlowSource.ANY}) @FlowSinks({FlowSink.LOG}) Object fsAny() {
+    private @Sources({FlowSource.ANY}) @Sinks({FlowSink.LOG}) Object fsAny() {
         return null;
     }
     ////:: error: (forbidden.flow)
-    public void timeToLogCat(final @FlowSources({FlowSource.TIME}) @FlowSinks({FlowSink.LOG}) Object obj) {
+    public void timeToLogCat(final @Sources({FlowSource.TIME}) @Sinks({FlowSink.LOG}) Object obj) {
     }
  //   //:: error: (forbidden.flow)
-    public void micToExt(final @FlowSources({FlowSource.MICROPHONE}) @FlowSinks({FlowSink.EXTERNAL_STORAGE}) Object obj) {
+    public void micToExt(final @Sources({FlowSource.MICROPHONE}) @Sinks({FlowSink.EXTERNAL_STORAGE}) Object obj) {
     }
  //   //:: error: (forbidden.flow)
-    public @FlowSources({FlowSource.MICROPHONE}) @FlowSinks({FlowSink.EXTERNAL_STORAGE}) Object micToExt2(final @FlowSources({FlowSource.MICROPHONE}) @FlowSinks({}) Object obj) {
+    public @Sources({FlowSource.MICROPHONE}) @Sinks({FlowSink.EXTERNAL_STORAGE}) Object micToExt2(final @Sources({FlowSource.MICROPHONE}) @Sinks({}) Object obj) {
         return null;
     }
    //  //:: error: (forbidden.flow)
-    public @FlowSources(FlowSource.ANY) @FlowSinks(FlowSink.EXTERNAL_STORAGE) double fromAny(@FlowSources(FlowSource.LOCATION)  @FlowSinks(FlowSink.EXTERNAL_STORAGE) int x) {
+    public @Sources(FlowSource.ANY) @Sinks(FlowSink.EXTERNAL_STORAGE) double fromAny(@Sources(FlowSource.LOCATION)  @Sinks(FlowSink.EXTERNAL_STORAGE) int x) {
       //  //:: error: (forbidden.flow)
-    	return (@FlowSources(FlowSource.ANY) @FlowSinks(FlowSink.EXTERNAL_STORAGE)  double) x;
+    	return (@Sources(FlowSource.ANY) @Sinks(FlowSink.EXTERNAL_STORAGE)  double) x;
     }
 
   //  //:: error: (forbidden.flow)
-    public <T extends @FlowSources({FlowSource.ACCOUNTS}) @FlowSinks({FlowSink.NETWORK}) Object> T accToNet() { return null; }
+    public <T extends @Sources({FlowSource.ACCOUNTS}) @Sinks({FlowSink.NETWORK}) Object> T accToNet() { return null; }
     // //:: error: (forbidden.flow)
-    public List<? extends @FlowSources({FlowSource.ACCOUNTS}) @FlowSinks({FlowSink.FILESYSTEM}) File> accFileToNet() { return null; }
+    public List<? extends @Sources({FlowSource.ACCOUNTS}) @Sinks({FlowSink.FILESYSTEM}) File> accFileToNet() { return null; }
 
     void test() {
        //  //:: error: (forbidden.flow)
-        final @FlowSources({FlowSource.PHONE_NUMBER}) @FlowSinks({FlowSink.LOG}) Object obj = null;
+        final @Sources({FlowSource.PHONE_NUMBER}) @Sinks({FlowSink.LOG}) Object obj = null;
        //  //:: error: (forbidden.flow)
-        final @FlowSources({FlowSource.TIME}) @FlowSinks({FlowSink.LOG}) Object anyObj = null;
+        final @Sources({FlowSource.TIME}) @Sinks({FlowSink.LOG}) Object anyObj = null;
         // //:: error: (forbidden.flow)
-        final @FlowSources({FlowSource.MICROPHONE}) @FlowSinks({FlowSink.LOG}) Object micToLc = null;
+        final @Sources({FlowSource.MICROPHONE}) @Sinks({FlowSink.LOG}) Object micToLc = null;
 
        // //:: error: (forbidden.flow)
-        final @FlowSources({FlowSource.ACCOUNTS}) @FlowSinks({FlowSink.EMAIL}) Object accToEm = null;
+        final @Sources({FlowSource.ACCOUNTS}) @Sinks({FlowSink.EMAIL}) Object accToEm = null;
        //  //:: error: (forbidden.flow)
-        final @FlowSources({FlowSource.ACCOUNTS}) @FlowSinks({FlowSink.DISPLAY}) Object accToDi = null;
+        final @Sources({FlowSource.ACCOUNTS}) @Sinks({FlowSink.DISPLAY}) Object accToDi = null;
         // //:: error: (forbidden.flow)
-        final String [] arrayOfAccToDEm = new String @FlowSources({FlowSource.ACCOUNTS}) @FlowSinks({FlowSink.DISPLAY, FlowSink.EMAIL}) []{};
+        final String [] arrayOfAccToDEm = new String @Sources({FlowSource.ACCOUNTS}) @Sinks({FlowSink.DISPLAY, FlowSink.EMAIL}) []{};
 
         // //:: error: (forbidden.flow)
-        final String [] arrayOfAccToDEmFs = new String @FlowSources({FlowSource.ACCOUNTS}) @FlowSinks({FlowSink.DISPLAY, FlowSink.EMAIL, FlowSink.FILESYSTEM}) []{};
+        final String [] arrayOfAccToDEmFs = new String @Sources({FlowSource.ACCOUNTS}) @Sinks({FlowSink.DISPLAY, FlowSink.EMAIL, FlowSink.FILESYSTEM}) []{};
         // //:: error: (forbidden.flow)
-        final @FlowSources({FlowSource.ACCOUNTS}) @FlowSinks({FlowSink.FILESYSTEM}) Object accToFs = null;
+        final @Sources({FlowSource.ACCOUNTS}) @Sinks({FlowSink.FILESYSTEM}) Object accToFs = null;
         // //:: error: (forbidden.flow)
-        final @FlowSources({FlowSource.ACCOUNTS}) @FlowSinks({FlowSink.FILESYSTEM, FlowSink.DISPLAY}) Object accToFsDi = null;
+        final @Sources({FlowSource.ACCOUNTS}) @Sinks({FlowSink.FILESYSTEM, FlowSink.DISPLAY}) Object accToFsDi = null;
 
 
         // //:: error: (forbidden.flow)
-        List<@FlowSources({FlowSource.MICROPHONE, FlowSource.TIME}) @FlowSinks({FlowSink.EMAIL, FlowSink.FILESYSTEM}) File> maTiFile = null;
+        List<@Sources({FlowSource.MICROPHONE, FlowSource.TIME}) @Sinks({FlowSink.EMAIL, FlowSink.FILESYSTEM}) File> maTiFile = null;
         // //:: error: (forbidden.flow)
-        List<@FlowSources({FlowSource.MICROPHONE, FlowSource.TIME}) @FlowSinks({FlowSink.EMAIL, FlowSink.NETWORK}) File> maTiFile2 = null;
+        List<@Sources({FlowSource.MICROPHONE, FlowSource.TIME}) @Sinks({FlowSink.EMAIL, FlowSink.NETWORK}) File> maTiFile2 = null;
         // //:: error: (forbidden.flow)
-        List<@FlowSources({FlowSource.MICROPHONE}) @FlowSinks({FlowSink.EMAIL, FlowSink.NETWORK}) File> maFile2 = null;
+        List<@Sources({FlowSource.MICROPHONE}) @Sinks({FlowSink.EMAIL, FlowSink.NETWORK}) File> maFile2 = null;
 
         
-        @FlowSources({FlowSource.PHONE_NUMBER}) @FlowSinks({FlowSink.EMAIL, FlowSink.LOG})
+        @Sources({FlowSource.PHONE_NUMBER}) @Sinks({FlowSink.EMAIL, FlowSink.LOG})
         class Whatever {
-//:: error: receiver parameter not applicable for constructor of top-level class: @FlowSources(value = {FlowSource.PHONE_NUMBER}) 
-            public Whatever(@FlowSources({FlowSource.PHONE_NUMBER}) @FlowSinks({FlowSink.NETWORK}) Whatever this) {
+//:: error: receiver parameter not applicable for constructor of top-level class: @Sources(value = {FlowSource.PHONE_NUMBER}) 
+            public Whatever(@Sources({FlowSource.PHONE_NUMBER}) @Sinks({FlowSink.NETWORK}) Whatever this) {
 
             }
         }
 
       /// /:: error: (forbidden.flow)
-        final Object whatever = new @FlowSources({FlowSource.PHONE_NUMBER}) @FlowSinks({FlowSink.NETWORK}) Whatever();
+        final Object whatever = new @Sources({FlowSource.PHONE_NUMBER}) @Sinks({FlowSink.NETWORK}) Whatever();
       //// :: error: (forbidden.flow)
-        final @FlowSources({FlowSource.MICROPHONE}) @FlowSinks({FlowSink.RANDOM}) Object micToRandom = null;
+        final @Sources({FlowSource.MICROPHONE}) @Sinks({FlowSink.RANDOM}) Object micToRandom = null;
 
       ////:: error: (forbidden.flow)
-        final @FlowSources({FlowSource.MICROPHONE}) @FlowSinks({FlowSink.NETWORK, FlowSink.EXTERNAL_STORAGE}) Object micToNExt = null;
+        final @Sources({FlowSource.MICROPHONE}) @Sinks({FlowSink.NETWORK, FlowSink.EXTERNAL_STORAGE}) Object micToNExt = null;
       ////:: error: (forbidden.flow) 
-        final @FlowSources({FlowSource.MICROPHONE}) @FlowSinks({FlowSink.NETWORK, FlowSink.LOG, FlowSink.SMS}) Object micToNetLogMsg = null;
+        final @Sources({FlowSource.MICROPHONE}) @Sinks({FlowSink.NETWORK, FlowSink.LOG, FlowSink.SMS}) Object micToNetLogMsg = null;
     }
 
     public static void testInstantiate() {
