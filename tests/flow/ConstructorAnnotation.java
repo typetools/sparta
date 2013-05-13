d1c9a89dd346964a9e4ac3a890eece7c0f0251b4
David McArthur <mcarthur@cs.washington.edu>
2013-05-13 14:50:06 -0700

    Adding tests for constructor annotations
    This tests @ConserviativeFlow on constructors for parameters to and return types of constructors.
    
    This also tests explicit return type annotations of constructors.
    
    The tests have currently been made to pass to not break builds. There are notes on how the tests should fail. After review the tests will be enabled to test fixes / find regressions.

---
+++ tests/flow/ConstructorAnnotation.java
@@ -0,0 +1,54 @@
+import sparta.checkers.quals.ConservativeFlow;
+import sparta.checkers.quals.Source;
+import static sparta.checkers.quals.FlowPermission.*;
+
+@ConservativeFlow
+class TestImplicitConstructor { }
+
+class TestNoParamConstructor {
+	@ConservativeFlow
+	TestNoParamConstructor() { }
+}
+
+@ConservativeFlow
+class TestParamConstructor {
+	
+	@ConservativeFlow
+	//:: error: (forbidden.flow)   
+	TestParamConstructor(String name) { }
+	
+	//:: error: (forbidden.flow)
+	static void test(String test) { }
+}
+
+class TestExplicitConstructorType {
+	 /* @Source(INTERNET) */
+	 TestExplicitConstructorType()  { }
+}
+
+
+class ConstructorAnnotation {
+
+	void testConstructor() {
+		
+		// BUG? This should be thrown //:: error: (assignment.type.incompatible) 
+		@Source(LITERAL) TestImplicitConstructor imp = new TestImplicitConstructor();
+		// BUG? This should be thrown //:: error: (assignment.type.incompatible)
+		@Source(LITERAL) TestNoParamConstructor noParam = new TestNoParamConstructor();
+		// BUG? This should be thrown //:: error: (assignment.type.incompatible)
+		@Source(LITERAL) TestParamConstructor param = new TestParamConstructor("hello");
+		// BUG? There should be an error on passing in a literal parameter
+		new TestParamConstructor("hello");
+		
+		// Sanity check -- OK
+		//:: error: (argument.type.incompatible)
+		TestParamConstructor.test("test");
+		
+		// BUG? This error should be thrown //:: error: (assignment.type.incompatible)
+		@Source(CAMERA) TestExplicitConstructorType explicit = new TestExplicitConstructorType();
+
+		// Until recently this threw an error about constructor invocation...
+		// //:: error: (constructor.invocation.invalid)
+		@Source(INTERNET) TestExplicitConstructorType explicit2 = new TestExplicitConstructorType();
+	}
+}
