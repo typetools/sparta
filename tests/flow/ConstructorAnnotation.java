6b96a22b080f719013ff8161f5c901b42422efef
David McArthur <mcarthur@cs.washington.edu>
2013-05-16 12:38:54 -0700

    Made constructor return type tests more specific.

--- tests/flow/ConstructorAnnotation.java
+++ tests/flow/ConstructorAnnotation.java
@@ -1,5 +1,7 @@
 import sparta.checkers.quals.ConservativeFlow;
 import sparta.checkers.quals.Source;
+import sparta.checkers.quals.Sink;
+
 import static sparta.checkers.quals.FlowPermission.*;
 
 @ConservativeFlow
@@ -20,8 +22,13 @@ class TestParamConstructor {
 }
 
 class TestExplicitConstructorType {
-	 /*@Source(INTERNET)*/
+	 @Source(INTERNET) @Sink(CAMERA)
 	 TestExplicitConstructorType()  { }
+}
+
+@Source(INTERNET) @Sink(CAMERA)
+class TestClassAnnotationType {
+	TestClassAnnotationType()  { }
 }
 
 
@@ -29,27 +36,37 @@ class ConstructorAnnotation {
 
 	void testConstructor() {
 		
+		// Sanity check -- OK
+		//:: error: (argument.type.incompatible)
+		TestParamConstructor.test("test");
+
+		// OK -- Conservative flow on parameters
+		//:: error: (argument.type.incompatible)
+		new TestParamConstructor("hello");
+		
+		// BUG? Class annotations dont seem to do anything.
+		//:: error: (assignment.type.incompatible)
+		@Source(INTERNET) @Sink(CAMERA) TestClassAnnotationType classAnnotation = new TestClassAnnotationType();
+		
+		// BUG? Constructor annotations
+		// We get invocation.invalid here based on the reciever type.
+		// Is TestExplicitConstructor constructor annotation doing the correct thing?
+		// 
+		// error: creation of @Sink(FlowPermission.CAMERA) @Source(FlowPermission.INTERNET) void 
+		// <init>(@Sink(CONDITIONAL) @Source(LITERAL) TestExplicitConstructorType this) 
+		// not allowed with given receiver;
+		// 
+		// found   : @Sink(FlowPermission.CAMERA) @Source(FlowPermission.INTERNET) TestExplicitConstructorType
+		// required: @Sink(CONDITIONAL) @Source(LITERAL) TestExplicitConstructorType
+		// :: error: (constructor.invocation.invalid)
+		new TestExplicitConstructorType();
+		
+		// Conservative flow return types.
+		
 		// BUG? This should be thrown //:: error: (assignment.type.incompatible) 
 		@Source(LITERAL) TestImplicitConstructor imp = new TestImplicitConstructor();
 		// BUG? This should be thrown //:: error: (assignment.type.incompatible)
 		@Source(LITERAL) TestNoParamConstructor noParam = new TestNoParamConstructor();
 
-		//:: error: (argument.type.incompatible)
-		@Source(LITERAL) TestParamConstructor param = new TestParamConstructor("hello");
-		
-		//:: error: (argument.type.incompatible)
-		new TestParamConstructor("hello");
-		
-		// Sanity check -- OK
-		//:: error: (argument.type.incompatible)
-		TestParamConstructor.test("test");
-		
-		// BUG? This error should be thrown 
-		//:: error: (assignment.type.incompatible)
-		@Source(CAMERA) TestExplicitConstructorType explicit = new TestExplicitConstructorType();
-
-		// Until recently this threw an error about constructor invocation...
-		//:: error: (constructor.invocation.invalid)
-		@Source(INTERNET) TestExplicitConstructorType explicit2 = new TestExplicitConstructorType();
 	}
 }
