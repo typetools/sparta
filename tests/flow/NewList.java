46e65191e770b1595c463043dd66e8ce792e428b
Suzanne Millstein <smillst@cs.washington.edu>
2013-05-17 11:17:00 -0700

    Added annotations for List and ArrayList

--- tests/flow/NewList.java
+++ tests/flow/NewList.java
@@ -2,16 +2,18 @@ import java.util.*;
 
 import sparta.checkers.quals.*;
 import sparta.checkers.quals.FlowPermission;
+import static sparta.checkers.quals.FlowPermission.*;
+
 
 class MyList<T extends @Source(sparta.checkers.quals.FlowPermission.ANY) Object> {
 }
 
 class NewList {
-    List<@Source(FlowPermission.ACCESS_FINE_LOCATION) Object> good1  = new ArrayList<@Source(FlowPermission.ACCESS_FINE_LOCATION) Object>();
+    List<@Source(ACCESS_FINE_LOCATION) Object> good1  = new ArrayList<@Source(ACCESS_FINE_LOCATION) Object>();
     //:: error: (assignment.type.incompatible)
-    List<@Source(FlowPermission.ACCESS_FINE_LOCATION) Object> bad1 = new ArrayList<@Source(FlowPermission.ANY) Object>();
+    List<@Source(ACCESS_FINE_LOCATION) Object> bad1 = new ArrayList<@Source(ANY) Object>();
 
-    MyList</*@Source(FlowPermission.ACCESS_FINE_LOCATION)*/ Object> good2  = new MyList<@Source(FlowPermission.ACCESS_FINE_LOCATION) Object>();
+    MyList</*@Source(FlowPermission.ACCESS_FINE_LOCATION)*/ Object> good2  = new MyList<@Source(ACCESS_FINE_LOCATION) Object>();
     //:: error: (assignment.type.incompatible)
-    MyList<@Source(FlowPermission.ACCESS_FINE_LOCATION) Object> bad2 = new MyList<@Source(FlowPermission.ANY) Object>();
+    MyList<@Source(ACCESS_FINE_LOCATION) Object> bad2 = new MyList<@Source(FlowPermission.ANY) Object>();
 }
