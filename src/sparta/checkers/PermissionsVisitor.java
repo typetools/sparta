7f1fd87ea9caff18fa7aa2dc5a6c5fae9da99f45
Paulo Barros <pbsf@cs.washington.edu>
2013-06-11 08:36:21 -0700

    Adding annotation @MayRequiredPermission on source and manual. Removing gson JARs from classpath.

--- src/sparta/checkers/PermissionsVisitor.java
+++ src/sparta/checkers/PermissionsVisitor.java
@@ -6,64 +6,108 @@ import java.util.List;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.ExecutableElement;
 
+import sparta.checkers.quals.MayRequiredPermissions;
 import sparta.checkers.quals.RequiredPermissions;
-
-import com.sun.source.tree.CompilationUnitTree;
-import com.sun.source.tree.MethodInvocationTree;
-import com.sun.source.tree.MethodTree;
-
 import checkers.basetype.BaseTypeVisitor;
 import checkers.source.Result;
 import checkers.util.AnnotationUtils;
 import checkers.util.TreeUtils;
 
+import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.MethodTree;
+
 /**
- * Propagate required permissions up the call stack.
- * Require that they are declared in the manifest.
- *
- * TODO: should we propagate required permissions from (anonymous) inner classes to the outside?
+ * Propagate required permissions up the call stack. Require that they are
+ * declared in the manifest.
+ * 
+ * TODO: should we propagate required permissions from (anonymous) inner classes
+ * to the outside?
  */
 public class PermissionsVisitor extends BaseTypeVisitor<PermissionsChecker> {
 
-    public PermissionsVisitor(PermissionsChecker checker,
-            CompilationUnitTree root) {
-        super(checker, root);
-    }
+	public PermissionsVisitor(PermissionsChecker checker,
+			CompilationUnitTree root) {
+		super(checker, root);
+	}
 
-    @Override
-    public Void visitMethodInvocation(MethodInvocationTree node, Void p) {
-        // Ensure that the enclosing method has at least @RequiredPermissions
-        ExecutableElement methodElt = TreeUtils.elementFromUse(node);
-        AnnotationMirror reqP = atypeFactory.getDeclAnnotation(methodElt, RequiredPermissions.class);
-        if (reqP!=null) {
-            List<String> reqPerms = AnnotationUtils.getElementValueArray(reqP, "value", String.class, false);
-            if (!reqPerms.isEmpty()) {
-                ExecutableElement callerElt = TreeUtils.elementFromDeclaration(TreeUtils.enclosingMethod(getCurrentPath()));
-                AnnotationMirror callerReq = atypeFactory.getDeclAnnotation(callerElt, RequiredPermissions.class);
-                List<String> callerPerms;
-                List<String> missing = new LinkedList<String>();
-                if (callerReq==null) {
-                    missing.addAll(reqPerms);
-                    callerPerms = new LinkedList<String>();
-                } else {
-                    callerPerms = AnnotationUtils.getElementValueArray(callerReq, "value", String.class, false);
-                    for (String perm : reqPerms) {
-                        if (!callerPerms.contains(perm)) {
-                            missing.add(perm);
-                        }
-                    }
-                }
-                if (!missing.isEmpty()) {
-                    checker.report(Result.failure("unsatisfied.permissions", missing, callerPerms), node);
-                }
-            }
-        }
-        return super.visitMethodInvocation(node, p);
-    }
+	@Override
+	public Void visitMethodInvocation(MethodInvocationTree node, Void p) {
+		// Look for @RequiredPermissions on the enclosing method
+		ExecutableElement methodElt = TreeUtils.elementFromUse(node);
+		AnnotationMirror reqP = atypeFactory.getDeclAnnotation(methodElt,
+				RequiredPermissions.class);
+		if (reqP != null) {
+			List<String> reqPerms = AnnotationUtils.getElementValueArray(reqP,
+					"value", String.class, false);
+			if (!reqPerms.isEmpty()) {
+				ExecutableElement callerElt = TreeUtils
+						.elementFromDeclaration(TreeUtils
+								.enclosingMethod(getCurrentPath()));
+				AnnotationMirror callerReq = atypeFactory.getDeclAnnotation(
+						callerElt, RequiredPermissions.class);
+				List<String> callerPerms;
+				List<String> missing = new LinkedList<String>();
+				if (callerReq == null) {
+					missing.addAll(reqPerms);
+					callerPerms = new LinkedList<String>();
+				} else {
+					callerPerms = AnnotationUtils.getElementValueArray(
+							callerReq, "value", String.class, false);
+					for (String perm : reqPerms) {
+						if (!callerPerms.contains(perm)) {
+							missing.add(perm);
+						}
+					}
+				}
+				if (!missing.isEmpty()) {
+					checker.report(Result.failure("unsatisfied.permissions",
+							missing, callerPerms), node);
+				}
+			}
+		}
+		// Look for @MayRequiredPermissions on the enclosing method
+		AnnotationMirror mayReqP = atypeFactory.getDeclAnnotation(methodElt,
+				MayRequiredPermissions.class);
+		if (mayReqP != null) {
+			List<String> mayReqPerms = AnnotationUtils.getElementValueArray(
+					mayReqP, "value", String.class, false);
+			if (!mayReqPerms.isEmpty()) {
+				ExecutableElement callerElt = TreeUtils
+						.elementFromDeclaration(TreeUtils
+								.enclosingMethod(getCurrentPath()));
+				AnnotationMirror callerReq = atypeFactory.getDeclAnnotation(
+						callerElt, MayRequiredPermissions.class);
+				List<String> callerPerms;
+				List<String> missing = new LinkedList<String>();
+				if (callerReq == null) {
+					missing.addAll(mayReqPerms);
+					callerPerms = new LinkedList<String>();
+				} else {
+					callerPerms = AnnotationUtils.getElementValueArray(
+							callerReq, "value", String.class, false);
+					for (String perm : mayReqPerms) {
+						if (!callerPerms.contains(perm)) {
+							missing.add(perm);
+						}
+					}
+				}
+				if (!missing.isEmpty()) {
+					checker.report(Result.failure("may.required.permissions",
+							missing, callerPerms, AnnotationUtils
+									.getElementValue(mayReqP, "notes",
+											String.class, false)), node);
+				}
+			}
 
-    @Override
-    public Void visitMethod(MethodTree node, Void p) {
-        // Ensure that all constants in @RequiredPermissions are in Manifest
-        return super.visitMethod(node, p);
-    }
+		}
+
+		return super.visitMethodInvocation(node, p);
+	}
+
+	@Override
+	public Void visitMethod(MethodTree node, Void p) {
+		// Ensure that all constants in @RequiredPermissions are in Manifest
+		return super.visitMethod(node, p);
+	}
 }
---
+++ src/sparta/checkers/quals/MayRequiredPermissions.java
@@ -0,0 +1,17 @@
+package sparta.checkers.quals;
+
+import java.lang.annotation.*;
+/**
+ * List of Android permissions that may be required in some cases to use a method.
+ * 
+ *
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD})
+public @interface MayRequiredPermissions {
+    // TODO: the annotation is not recognized if it's in a comment!
+    /*@Permission*/ String[] value() default {};
+    //Method notes() should contain an explanation on when the permission is required
+    String notes() default "";
+}
