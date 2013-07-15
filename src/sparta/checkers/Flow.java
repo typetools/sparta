d7bca54208ff85149708c24e0c8b105180164896
Suzanne Millstein <smillst@cs.washington.edu>
2013-07-15 11:34:54 -0700

    Fixed whitespace

--- src/sparta/checkers/Flow.java
+++ src/sparta/checkers/Flow.java
@@ -7,73 +7,73 @@ import sparta.checkers.quals.FlowPermission;
 import sparta.checkers.quals.FlowPermission;
 
 public class Flow {
-	Set<FlowPermission> sources;
-	Set<FlowPermission> sinks;
+    Set<FlowPermission> sources;
+    Set<FlowPermission> sinks;
 
-	public Flow(Set<FlowPermission> sources, Set<FlowPermission> sinks) {
-		this.sources = sources;
-		this.sinks = sinks;
-	}
+    public Flow(Set<FlowPermission> sources, Set<FlowPermission> sinks) {
+        this.sources = sources;
+        this.sinks = sinks;
+    }
 
-	public Flow(FlowPermission source, Set<FlowPermission> sinks) {
-		this.sources = new HashSet<FlowPermission>();
-		if(source != null){
-		sources.add(source);
-		}
-		this.sinks = FlowUtil.allToAnySink(sinks, false);
-	}
+    public Flow(FlowPermission source, Set<FlowPermission> sinks) {
+        this.sources = new HashSet<FlowPermission>();
+        if (source != null) {
+            sources.add(source);
+        }
+        this.sinks = FlowUtil.allToAnySink(sinks, false);
+    }
 
-	public Flow(Set<FlowPermission> sources, FlowPermission sink) {
-		this.sources = FlowUtil.allToAnySource(sources, false);
-		this.sinks = new HashSet<FlowPermission>();
-		sinks.add(sink);
-	}
+    public Flow(Set<FlowPermission> sources, FlowPermission sink) {
+        this.sources = FlowUtil.allToAnySource(sources, false);
+        this.sinks = new HashSet<FlowPermission>();
+        sinks.add(sink);
+    }
 
-	public Flow(Set<FlowPermission> sources) {
-		this.sinks = new HashSet<FlowPermission>();
-		this.sources = sources;
-	}
+    public Flow(Set<FlowPermission> sources) {
+        this.sinks = new HashSet<FlowPermission>();
+        this.sources = sources;
+    }
 
-	public Flow() {
-		this.sinks = new HashSet<FlowPermission>();
-		this.sources = new HashSet<FlowPermission>();
-	}
+    public Flow() {
+        this.sinks = new HashSet<FlowPermission>();
+        this.sources = new HashSet<FlowPermission>();
+    }
 
-	public Flow(FlowPermission source) {
-		this.sinks = new HashSet<FlowPermission>();
-		this.sources = new HashSet<FlowPermission>();
-		sources.add(source);
-	}
+    public Flow(FlowPermission source) {
+        this.sinks = new HashSet<FlowPermission>();
+        this.sources = new HashSet<FlowPermission>();
+        sources.add(source);
+    }
 
-	public String toString() {
-		StringBuffer flow = new StringBuffer();
-		if (sources.isEmpty()) {
-			flow.append(" {}");
-		} else {
-			flow.append(sources);
-		}
-		flow.append("->");
-		if (sinks.isEmpty()) {
-			flow.append(" {}");
-		} else {
-			flow.append(sinks);
-		}
-		String flowstring = flow.toString().replace('[', ' ');
-		flowstring = flowstring.toString().replace(']', ' ');
-		return flowstring;
-	}
+    public String toString() {
+        StringBuffer flow = new StringBuffer();
+        if (sources.isEmpty()) {
+            flow.append(" {}");
+        } else {
+            flow.append(sources);
+        }
+        flow.append("->");
+        if (sinks.isEmpty()) {
+            flow.append(" {}");
+        } else {
+            flow.append(sinks);
+        }
+        String flowstring = flow.toString().replace('[', ' ');
+        flowstring = flowstring.toString().replace(']', ' ');
+        return flowstring;
+    }
 
-	public void addSink(FlowPermission sink) {
-		sinks.add(sink);
-	}
+    public void addSink(FlowPermission sink) {
+        sinks.add(sink);
+    }
 
-	public boolean hasSink() {
-		return !sinks.isEmpty();
-	}
+    public boolean hasSink() {
+        return !sinks.isEmpty();
+    }
 
-	public void addSink(Set<FlowPermission> sinks) {
-		this.sinks.addAll(FlowUtil.allToAnySink(sinks, false));
-		
-	}
+    public void addSink(Set<FlowPermission> sinks) {
+        this.sinks.addAll(FlowUtil.allToAnySink(sinks, false));
+
+    }
 
 }
