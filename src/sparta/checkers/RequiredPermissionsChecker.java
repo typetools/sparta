4ae96106111c4cc70034c5a3b122fc7b3839379c
Suzanne Millstein <smillst@cs.washington.edu>
2013-07-24 08:54:55 -0700

    More missing classes

---
+++ src/sparta/checkers/RequiredPermissionsChecker.java
@@ -0,0 +1,10 @@
+package sparta.checkers;
+
+import checkers.basetype.BaseTypeChecker;
+import checkers.quals.StubFiles;
+import checkers.quals.TypeQualifiers;
+import checkers.quals.Unqualified;
+
+@TypeQualifiers(Unqualified.class)
+@StubFiles("permission.astub")
+public class RequiredPermissionsChecker extends BaseTypeChecker {}
