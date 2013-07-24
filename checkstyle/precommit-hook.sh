9dbcdccb91e4b157fca35c364eae266ea962d0f1
rjust <devnull@localhost>
2013-07-23 22:40:34 -0700

    Pre-commit hook and checkstyle added to run it on every touched source file before the actual commit

---
+++ checkstyle/precommit-hook.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+files=`hg st | sed -n "s/^[AM]\s\(.*\.java$\)/\1/p" | tr '\n' ' '`
+[ -z "$files" ] && exit 0
+
+java -jar checkstyle/checkstyle-5.6-all.jar \
+          -c checkstyle/sun_checks.xml \
+          $files
+if [ $? -ne 0 ]
+then
+    echo "Committed source code does not comply with style guidelines!"
+    exit 1
+fi
