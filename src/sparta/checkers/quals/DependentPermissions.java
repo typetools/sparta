62efa6851f28988a157f412a107e7892d67aeb6a
Suzanne Millstein <smillst@cs.washington.edu>
2013-07-26 08:24:57 -0700

    DependentPermissions must be allowed on methods

--- src/sparta/checkers/quals/DependentPermissions.java
+++ src/sparta/checkers/quals/DependentPermissions.java
@@ -12,14 +12,10 @@ import java.lang.annotation.*;
 import checkers.fenum.quals.FenumTop;
 import checkers.quals.*;
 
-/**
- * A generic fake enumeration qualifier that is parameterized by a name. It is
- * written in source code as, for example, <tt>@Fenum("cardSuit")</tt> and
- * <tt>@Fenum("faceValue")</tt>, which would be distinct fake enumerations.
- */
+
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.TYPE_USE, ElementType.TYPE_PARAMETER })
+@Target({ ElementType.TYPE_USE, ElementType.TYPE_PARAMETER, ElementType.CONSTRUCTOR, ElementType.METHOD })
 @TypeQualifier
 @SubtypeOf(FenumTop.class)
 public @interface DependentPermissions {
