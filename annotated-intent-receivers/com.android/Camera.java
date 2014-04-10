813183e4a3a5d134a2c85c0a6942361f70ca1837
Paulo Barros <pbsf@cs.washington.edu>
2014-04-10 15:17:39 -0300

    Adding Camera class to annotated intent receivers

---
+++ annotated-intent-receivers/com.android/Camera.java
@@ -0,0 +1,20 @@
+package com.android.camera;
+
+import sparta.checkers.quals.*;
+import android.app.Activity;
+import android.content.Intent;
+import android.provider.MediaStore;
+
+import static sparta.checkers.quals.FlowPermission.*;
+
+
+public class Camera extends Activity {
+
+	@Override
+	@ReceiveIntent("startActivity,1")
+    public @IntentMap({
+        @Extra(key = MediaStore.EXTRA_OUTPUT, source = { ANY }, sink = { INTERNET, FILESYSTEM }) }) Intent getIntent() {
+		//The sink is a content resolver URI which could map to anything. We assume DATABASE, INTERNET and FILESYSTEM.
+        return super.getIntent();
+    }
+}
