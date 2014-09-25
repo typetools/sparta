2c97f77f52471c4db21b48b105575df50e4035e3
Suzanne Millstein <smillst@cs.washington.edu>
2014-09-25 10:28:17 -0700

    Added PACKAGE_INFO permission and updated the stubfiles.

--- src/sparta/checkers/quals/FlowPermission.java
+++ src/sparta/checkers/quals/FlowPermission.java
@@ -52,6 +52,7 @@ public enum FlowPermission {
     READ_CLIPBOARD(T.SOURCE),
     SPEAKER(T.SINK), // Physical speaker / headphones 
     SENSOR(T.SOURCE), //See android.hardware.Sensor
+    PACKAGE_INFO(T.BOTH), //For data from/to android.content.pm.PackageManager
     
     /**
      * These are old sources or sinks that may or may not be of use
