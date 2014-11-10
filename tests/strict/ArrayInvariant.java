3f98e3d41d431bfbc5e6e23c8057fe97bfb4155a
Suzanne Millstein <smillst@cs.washington.edu>
2014-11-10 13:58:48 -0800

    Corrected test

--- tests/strict/ArrayInvariant.java
+++ tests/strict/ArrayInvariant.java
@@ -16,14 +16,11 @@ class ArrayInvariant {
        // enum values instead of VariableElement, which caused the array equals check to fail.
        // There was also an annotation values equals bug in AnnotationUtil.areSame when
        // the values of an annotation had a different order.
-        
-        //TODO: BUG!
-       //:: error: (assignment.type.incompatible)
        flowCompleted = notCompleted;
 
-       // This will fail if order matters in annotation values.
        @Source(CAMERA) @Sink({FILESYSTEM, INTERNET}) byte @Source(CAMERA) @Sink({FILESYSTEM, INTERNET})[] a = null;
        //TODO: BUG!
+       // This will fail if order matters in annotation values.
        //:: error: (assignment.type.incompatible)
        @Source(CAMERA) @Sink({INTERNET, FILESYSTEM}) byte @Source(CAMERA) @Sink({FILESYSTEM, INTERNET})[] b = a;
        
