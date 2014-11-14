5778053f40c637816522bc54730822f271a77515
Suzanne Millstein <smillst@cs.washington.edu>
2014-11-14 14:13:21 -0800

    Added FlowPermissionString class.

---
+++ src/sparta/checkers/quals/FlowPermissionString.java
@@ -0,0 +1,209 @@
+package sparta.checkers.quals;
+/**
+ * This class contains constant that correspond to the FlowPermission.
+ * These are to be used in @Source or @Sink annotations.
+ * @author smillst
+ *
+ */
+public class FlowPermissionString {
+    public static final String EXTRA_DEFAULT = "EXTRA_DEFAULT";
+    public static final String ANY = "ANY";
+    
+    public static final String CAMERA_SETTINGS = "CAMERA_SETTINGS";
+    public static final String DISPLAY = "DISPLAY";
+    public static final String FILESYSTEM = "FILESYSTEM";
+    public static final String RANDOM = "RANDOM";
+    public static final String READ_TIME = "READ_TIME";
+    public static final String USER_INPUT = "USER_INPUT";
+    public static final String WRITE_LOGS = "WRITE_LOGS";
+    public static final String DATABASE = "DATABASE";
+    public static final String SYSTEM_PROPERTIES = "SYSTEM_PROPERTIES";
+    public static final String MEDIA = "MEDIA";
+    public static final String READ_EMAIL = "READ_EMAIL";
+    public static final String WRITE_EMAIL = "WRITE_EMAIL";
+    public static final String WRITE_CLIPBOARD = "WRITE_CLIPBOARD";
+    public static final String READ_CLIPBOARD = "READ_CLIPBOARD";
+    public static final String SPEAKER = "SPEAKER";
+    public static final String SENSOR = "SENSOR";
+    public static final String PACKAGE_INFO = "PACKAGE_INFO";
+    public static final String PHONE_NUMBER = "PHONE_NUMBER";
+    public static final String SHARED_PREFERENCES = "SHARED_PREFERENCES";
+    public static final String ACCELEROMETER = "ACCELEROMETER";
+    
+    /**
+     * The following permissions are temporary and implemented now in a simple
+     * way for an upcoming engagement.
+     */
+    public static final String REFLECTION = "REFLECTION";
+    public static final String INTENT = "INTENT";
+    public static final String BUNDLE = "BUNDLE";
+    public static final String PROCESS_BUILDER = "PROCESS_BUILDER";
+    public static final String PARCEL = "PARCEL";
+    public static final String SECURE_HASH = "SECURE_HASH";
+    public static final String CONTENT_PROVIDER = "CONTENT_PROVIDER";
+    
+    /**
+     * Android Manifest.permissions (Do not add new permissions below here) I
+     * was mostly guessing whether the permissions should be source, sink or
+     * both, so feel free to change the T of the permission. -SOM
+     */
+    
+    public static final String ACCESS_CHECKIN_PROPERTIES = "ACCESS_CHECKIN_PROPERTIES";
+    public static final String ACCESS_COARSE_LOCATION = "ACCESS_COARSE_LOCATION";
+    public static final String ACCESS_FINE_LOCATION = "ACCESS_FINE_LOCATION";
+    public static final String ACCESS_LOCATION_EXTRA_COMMANDS = "ACCESS_LOCATION_EXTRA_COMMANDS";
+    public static final String ACCESS_MOCK_LOCATION = "ACCESS_MOCK_LOCATION";
+    public static final String ACCESS_NETWORK_STATE = "ACCESS_NETWORK_STATE";
+    public static final String ACCESS_SURFACE_FLINGER = "ACCESS_SURFACE_FLINGER";
+    public static final String ACCESS_WIFI_STATE = "ACCESS_WIFI_STATE";
+    public static final String ACCOUNT_MANAGER = "ACCOUNT_MANAGER";
+    public static final String ADD_VOICEMAIL = "ADD_VOICEMAIL";
+    public static final String AUTHENTICATE_ACCOUNTS = "AUTHENTICATE_ACCOUNTS";
+    public static final String BATTERY_STATS = "BATTERY_STATS";
+    public static final String BIND_ACCESSIBILITY_SERVICE = "BIND_ACCESSIBILITY_SERVICE";
+    public static final String BIND_APPWIDGET = "BIND_APPWIDGET";
+    public static final String BIND_DEVICE_ADMIN = "BIND_DEVICE_ADMIN";
+    public static final String BIND_INPUT_METHOD = "BIND_INPUT_METHOD";
+    public static final String BIND_REMOTEVIEWS = "BIND_REMOTEVIEWS";
+    public static final String BIND_TEXT_SERVICE = "BIND_TEXT_SERVICE";
+    public static final String BIND_VPN_SERVICE = "BIND_VPN_SERVICE";
+    public static final String BIND_WALLPAPER = "BIND_WALLPAPER";
+    public static final String BLUETOOTH = "BLUETOOTH";
+    public static final String BLUETOOTH_ADMIN = "BLUETOOTH_ADMIN";
+    public static final String BRICK = "BRICK";
+    public static final String BROADCAST_PACKAGE_REMOVED = "BROADCAST_PACKAGE_REMOVED";
+    public static final String BROADCAST_SMS = "BROADCAST_SMS";
+    public static final String BROADCAST_STICKY = "BROADCAST_STICKY";
+    public static final String BROADCAST_WAP_PUSH = "BROADCAST_WAP_PUSH";
+    public static final String CALL_PHONE = "CALL_PHONE";
+    public static final String CALL_PRIVILEGED = "CALL_PRIVILEGED";
+    public static final String CAMERA = "CAMERA";
+    public static final String CHANGE_COMPONENT_ENABLED_STATE = "CHANGE_COMPONENT_ENABLED_STATE";
+    public static final String CHANGE_CONFIGURATION = "CHANGE_CONFIGURATION";
+    public static final String CHANGE_NETWORK_STATE = "CHANGE_NETWORK_STATE";
+    public static final String CHANGE_WIFI_MULTICAST_STATE = "CHANGE_WIFI_MULTICAST_STATE";
+    public static final String CHANGE_WIFI_STATE = "CHANGE_WIFI_STATE";
+    public static final String CLEAR_APP_CACHE = "CLEAR_APP_CACHE";
+    public static final String CLEAR_APP_USER_DATA = "CLEAR_APP_USER_DATA";
+    public static final String CONTROL_LOCATION_UPDATES = "CONTROL_LOCATION_UPDATES";
+    public static final String DELETE_CACHE_FILES = "DELETE_CACHE_FILES";
+    public static final String DELETE_PACKAGES = "DELETE_PACKAGES";
+    public static final String DEVICE_POWER = "DEVICE_POWER";
+    public static final String DIAGNOSTIC = "DIAGNOSTIC";
+    public static final String DISABLE_KEYGUARD = "DISABLE_KEYGUARD";
+    public static final String DUMP = "DUMP";
+    public static final String EXPAND_STATUS_BAR = "EXPAND_STATUS_BAR";
+    public static final String FACTORY_TEST = "FACTORY_TEST";
+    public static final String FLASHLIGHT = "FLASHLIGHT";
+    public static final String FORCE_BACK = "FORCE_BACK";
+    public static final String GET_ACCOUNTS = "GET_ACCOUNTS";
+    public static final String GET_PACKAGE_SIZE = "GET_PACKAGE_SIZE";
+    public static final String GET_TASKS = "GET_TASKS";
+    public static final String GLOBAL_SEARCH = "GLOBAL_SEARCH";
+    public static final String HARDWARE_TEST = "HARDWARE_TEST";
+    public static final String INJECT_EVENTS = "INJECT_EVENTS";
+    public static final String INSTALL_LOCATION_PROVIDER = "INSTALL_LOCATION_PROVIDER";
+    public static final String INSTALL_PACKAGES = "INSTALL_PACKAGES";
+    public static final String INTERNAL_SYSTEM_WINDOW = "INTERNAL_SYSTEM_WINDOW";
+    public static final String INTERNET = "INTERNET";
+    public static final String KILL_BACKGROUND_PROCESSES = "KILL_BACKGROUND_PROCESSES";
+    public static final String MANAGE_ACCOUNTS = "MANAGE_ACCOUNTS";
+    public static final String MANAGE_APP_TOKENS = "MANAGE_APP_TOKENS";
+    public static final String MASTER_CLEAR = "MASTER_CLEAR";
+    public static final String MODIFY_AUDIO_SETTINGS = "MODIFY_AUDIO_SETTINGS";
+    public static final String MODIFY_PHONE_STATE = "MODIFY_PHONE_STATE";
+    public static final String MOUNT_FORMAT_FILESYSTEMS = "MOUNT_FORMAT_FILESYSTEMS";
+    public static final String MOUNT_UNMOUNT_FILESYSTEMS = "MOUNT_UNMOUNT_FILESYSTEMS";
+    public static final String NFC = "NFC";
+    public static final String PERSISTENT_ACTIVITY = "PERSISTENT_ACTIVITY";
+    public static final String PROCESS_OUTGOING_CALLS = "PROCESS_OUTGOING_CALLS";
+    public static final String READ_CALENDAR = "READ_CALENDAR";
+    public static final String READ_CALL_LOG = "READ_CALL_LOG";
+    public static final String READ_CONTACTS = "READ_CONTACTS";
+    public static final String READ_EXTERNAL_STORAGE = "READ_EXTERNAL_STORAGE";
+    public static final String READ_FRAME_BUFFER = "READ_FRAME_BUFFER";
+    public static final String READ_HISTORY_BOOKMARKS = "READ_HISTORY_BOOKMARKS";
+    public static final String READ_INPUT_STATE = "READ_INPUT_STATE";
+    public static final String READ_LOGS = "READ_LOGS";
+    public static final String READ_PHONE_STATE = "READ_PHONE_STATE";
+    public static final String READ_PROFILE = "READ_PROFILE";
+    public static final String READ_SMS = "READ_SMS";
+    public static final String READ_SOCIAL_STREAM = "READ_SOCIAL_STREAM";
+    public static final String READ_SYNC_SETTINGS = "READ_SYNC_SETTINGS";
+    public static final String READ_SYNC_STATS = "READ_SYNC_STATS";
+    public static final String READ_USER_DICTIONARY = "READ_USER_DICTIONARY";
+    public static final String REBOOT = "REBOOT";
+    public static final String RECEIVE_BOOT_COMPLETED = "RECEIVE_BOOT_COMPLETED";
+    public static final String RECEIVE_MMS = "RECEIVE_MMS";
+    public static final String RECEIVE_SMS = "RECEIVE_SMS";
+    public static final String RECEIVE_WAP_PUSH = "RECEIVE_WAP_PUSH";
+    public static final String RECORD_AUDIO = "RECORD_AUDIO";
+    public static final String REORDER_TASKS = "REORDER_TASKS";
+    public static final String RESTART_PACKAGES = "RESTART_PACKAGES";
+    public static final String SEND_SMS = "SEND_SMS";
+    public static final String SET_ACTIVITY_WATCHER = "SET_ACTIVITY_WATCHER";
+    public static final String SET_ALARM = "SET_ALARM";
+    public static final String SET_ALWAYS_FINISH = "SET_ALWAYS_FINISH";
+    public static final String SET_ANIMATION_SCALE = "SET_ANIMATION_SCALE";
+    public static final String SET_DEBUG_APP = "SET_DEBUG_APP";
+    public static final String SET_ORIENTATION = "SET_ORIENTATION";
+    public static final String SET_POINTER_SPEED = "SET_POINTER_SPEED";
+    public static final String SET_PREFERRED_APPLICATIONS = "SET_PREFERRED_APPLICATIONS";
+    public static final String SET_PROCESS_LIMIT = "SET_PROCESS_LIMIT";
+    public static final String SET_TIME = "SET_TIME";
+    public static final String SET_TIME_ZONE = "SET_TIME_ZONE";
+    public static final String SET_WALLPAPER = "SET_WALLPAPER";
+    public static final String SET_WALLPAPER_HINTS = "SET_WALLPAPER_HINTS";
+    public static final String SIGNAL_PERSISTENT_PROCESSES = "SIGNAL_PERSISTENT_PROCESSES";
+    public static final String STATUS_BAR = "STATUS_BAR";
+    public static final String SUBSCRIBED_FEEDS_READ = "SUBSCRIBED_FEEDS_READ";
+    public static final String SUBSCRIBED_FEEDS_WRITE = "SUBSCRIBED_FEEDS_WRITE";
+    public static final String SYSTEM_ALERT_WINDOW = "SYSTEM_ALERT_WINDOW";
+    public static final String UPDATE_DEVICE_STATS = "UPDATE_DEVICE_STATS";
+    public static final String USE_CREDENTIALS = "USE_CREDENTIALS";
+    public static final String USE_SIP = "USE_SIP";
+    public static final String VIBRATE = "VIBRATE";
+    public static final String WAKE_LOCK = "WAKE_LOCK";
+    public static final String WRITE_APN_SETTINGS = "WRITE_APN_SETTINGS";
+    public static final String WRITE_CALENDAR = "WRITE_CALENDAR";
+    public static final String WRITE_CALL_LOG = "WRITE_CALL_LOG";
+    public static final String WRITE_CONTACTS = "WRITE_CONTACTS";
+    public static final String WRITE_EXTERNAL_STORAGE = "WRITE_EXTERNAL_STORAGE";
+    public static final String WRITE_GSERVICES = "WRITE_GSERVICES";
+    public static final String WRITE_HISTORY_BOOKMARKS = "WRITE_HISTORY_BOOKMARKS";
+    public static final String WRITE_PROFILE = "WRITE_PROFILE";
+    public static final String WRITE_SECURE_SETTINGS = "WRITE_SECURE_SETTINGS";
+    public static final String WRITE_SETTINGS = "WRITE_SETTINGS";
+    public static final String WRITE_SMS = "WRITE_SMS";
+    public static final String WRITE_SOCIAL_STREAM = "WRITE_SOCIAL_STREAM";
+    public static final String WRITE_SYNC_SETTINGS = "WRITE_SYNC_SETTINGS";
+    public static final String WRITE_TIME = "WRITE_TIME";
+    public static final String WRITE_USER_DICTIONARY = "WRITE_USER_DICTIONARY";
+    public static final String SUSI_UNIQUE_IDENTIFIER = "SUSI_UNIQUE_IDENTIFIER";
+    public static final String SUSI_LOCATION_INFORMATION = "SUSI_LOCATION_INFORMATION";
+    public static final String SUSI_NETWORK_INFORMATION = "SUSI_NETWORK_INFORMATION";
+    public static final String SUSI_ACCOUNT_INFORMATION = "SUSI_ACCOUNT_INFORMATION";
+    public static final String SUSI_FILE_INFORMATION = "SUSI_FILE_INFORMATION";
+    public static final String SUSI_BLUETOOTH_INFORMATION = "SUSI_BLUETOOTH_INFORMATION";
+    public static final String SUSI_DATABASE_INFORMATION = "SUSI_DATABASE_INFORMATION";
+    public static final String SUSI_EMAIL = "SUSI_EMAIL";
+    public static final String SUSI_SYNCHRONIZATION_DATA = "SUSI_SYNCHRONIZATION_DATA";
+    public static final String SUSI_SMS_MMS = "SUSI_SMS_MMS";
+    public static final String SUSI_CONTACT_INFORMATION = "SUSI_CONTACT_INFORMATION";
+    public static final String SUSI_CALENDAR_INFORMATION = "SUSI_CALENDAR_INFORMATION";
+    public static final String SUSI_SYSTEM_SETTINGS = "SUSI_SYSTEM_SETTINGS";
+    public static final String SUSI_IMAGE = "SUSI_IMAGE";
+    public static final String SUSI_BROWSER_INFORMATION = "SUSI_BROWSER_INFORMATION";
+    public static final String SUSI_NFC = "SUSI_NFC";
+    public static final String SUSI_NO_CATEGORY = "SUSI_NO_CATEGORY";
+    public static final String SUSI_PHONE_CONNECTION = "SUSI_PHONE_CONNECTION";
+    public static final String SUSI_VOIP = "SUSI_VOIP";
+    public static final String SUSI_PHONE_STATE = "SUSI_PHONE_STATE";
+    public static final String SUSI_BLUETOOTH = "SUSI_BLUETOOTH";
+    public static final String SUSI_ACCOUNT_SETTINGS = "SUSI_ACCOUNT_SETTINGS";
+    public static final String SUSI_AUDIO = "SUSI_AUDIO";
+    public static final String SUSI_NETWORK = "SUSI_NETWORK";
+    public static final String SUSI_FILE = "SUSI_FILE";
+    public static final String SUSI_LOG = "SUSI_LOG";
+
+}
