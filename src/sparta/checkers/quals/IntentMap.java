62f66f30e922f14ae43af64d3c412db61aeacf7e
Suzanne Millstein <smillst@cs.washington.edu>
2016-06-13 08:11:54 -0700

    Make @Target valid for qualifiers

--- src/sparta/checkers/quals/IntentMap.java
+++ src/sparta/checkers/quals/IntentMap.java
@@ -13,9 +13,7 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.TYPE_PARAMETER, ElementType.TYPE_USE,
-/* The following only added to make Eclipse work. */
-ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD, ElementType.LOCAL_VARIABLE })
+@Target({ ElementType.TYPE_PARAMETER, ElementType.TYPE_USE })
 @SubtypeOf({})
 public @interface IntentMap {
     Extra[] value() default {};
