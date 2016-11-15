1eef1f8fd5dbc202a64363507a0e5e88011ed02e
Suzanne Millstein <smillst@cs.washington.edu>
2016-11-15 15:08:46 -0800

    Revert "Try travis"
    This reverts commit f1bf25360b59c2f468085aea04607778b26bd76e.

--- .travis-build.sh
+++ .travis-build.sh
@@ -4,27 +4,13 @@ ROOT=$TRAVIS_BUILD_DIR/..
 # Fail the whole script if any command fails
 set -e
 
-cd $ROOT
-export JAVA_HOME=`which javac|xargs readlink -f|xargs dirname|xargs dirname`
-export CHECKERFRAMEWORK=$ROOT/checker-framework/
-mkdir $ROOT/android-sdk
-export ANDROID_HOME=$ROOT/android-sdk
-
-## Get the Android SDK
-apt-get update
-touch tmp | add-apt-repository ppa:ubuntu-desktop/ubuntu-make
-apt-get update
-echo "Y" | apt-get install ubuntu-make
-umake android android-sdk --accept-license $ANDROID_HOME
-export PATH=$ANDROID_HOME/tools/:$PATH
-
-## Get the latests Android tools and API 25
-echo "y" | android update sdk -u -t "tool, platform-tool, android-25"
-
 ## Build Checker Framework
 (cd $ROOT && git clone --depth 1 https://github.com/typetools/checker-framework.git)
 # This also builds annotation-tools and jsr308-langtools
 (cd checker-framework/ && ./.travis-build-without-test.sh)
 
-cd sparta
-ant all-tests-nostubs
+## Build
+ant jar
+
+## Run tests
+ant all-tests
