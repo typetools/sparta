1e2265458d2ade694aedf100e92a799e5e0a878e
Suzanne Millstein <smillst@cs.washington.edu>
2018-02-06 13:42:47 -0800

    Adpate to Checker Framework changes.

--- src/sparta/checkers/util/IntentStatsAnnotatedTypeFactory.java
+++ src/sparta/checkers/util/IntentStatsAnnotatedTypeFactory.java
@@ -1,8 +1,8 @@
 package sparta.checkers.util;
-import org.checkerframework.checker.linear.LinearAnnotatedTypeFactory;
+import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 
-public class IntentStatsAnnotatedTypeFactory extends LinearAnnotatedTypeFactory{
+public class IntentStatsAnnotatedTypeFactory extends BaseAnnotatedTypeFactory{
 
     public IntentStatsAnnotatedTypeFactory(BaseTypeChecker checker) {
         super(checker);
